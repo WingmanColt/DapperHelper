708956b99d219b40477df1832e1502d9d20c08f0
WingmanColt <supp.alliedsource@gmail.com>
2022-07-31 10:44:14 +0300

    Update Service.cs

--- Service.cs
+++ Service.cs
@@ -1,24 +1,39 @@
- public async Task<OperationResult> CRUD(CreateJobInputModel viewModel, User user, CrudActionEnum action)
+ public async Task<OperationResult> CRUD(object parameters, JobCrudActionEnum action, bool AutoFindParams, string skipAttribute, User user)
         {
-            var jobs = new Jobs();
-            // jobs.Update(viewModel, ApproveType.Waiting, user);
-            jobs.StatementType = action.GetDisplayName();
+            var param = new DynamicParameters();
+
+            if (AutoFindParams)
+                param = DapperPropertiesHelper.AutoParameterFind(parameters, skipAttribute);
+            else
+                param.AddDynamicParams(parameters);
+
+            var model = new { StatementType = action.GetDisplayName() };
+            param.AddDynamicParams(model);
+
+
+            if (action.Equals(JobCrudActionEnum.Create) || action.Equals(JobCrudActionEnum.Update))
+            {
+                Create jobs = new Create();
+                jobs.Update((CreateJobInputModel)parameters, ApproveType.Waiting, user);
+
+                param.Add("@newId", dbType: DbType.Int32, size: 50, direction: ParameterDirection.Output);
+            }
+
+            if (action.Equals(JobCrudActionEnum.Create))
+                param.Add("Id", 0);
 
             using (IDbConnection connection = Connection)
             {
-                var parameters = DapperPropertiesHelper.AutoParameterFind(jobs);
-
-                if (action.Equals(JobCrudActionEnum.Create))
-                    parameters.Add("Id", 0);
-                else
-                    parameters.Add("Id", viewModel.Id);
-                        
                 try
                 {
                     connection.Open();
-                    await connection.ExecuteAsync("spJobCRUD", parameters, commandType: CommandType.StoredProcedure);
+                    var result = await connection.ExecuteScalarAsync<int>("spJobCRUD", param, commandType: CommandType.StoredProcedure);
                     connection.Close();
 
+                    if (action.Equals(JobCrudActionEnum.Create) || action.Equals(JobCrudActionEnum.Update))
+                    {
+                        OperationResult.SuccessResult("").Id = param.Get<int?>("@newId"); 
+                    }
                     return OperationResult.SuccessResult("");
                 }
                 catch (Exception ex)
@@ -28,16 +43,108 @@
             }
 
         }
-    public enum CrudActionEnum : int
+
+        
+        public async Task<IAsyncEnumerable<T>> GetAll<T>(object parameters, JobGetActionEnum state, bool AutoFindParams, string skipAttribute)
+        {
+            var param = new DynamicParameters();
+
+            if(AutoFindParams)
+                param = DapperPropertiesHelper.AutoParameterFind(parameters, skipAttribute);
+            else
+                param.AddDynamicParams(parameters);
+
+            var model = new { StatementType = state.GetDisplayName(), Id = 0 };
+            param.AddDynamicParams(model);
+            
+            using (IDbConnection connection = Connection)
+            {
+                connection.Open();
+                var result = await connection.QueryAsync<T>("spJobCRUD", param, commandType: CommandType.StoredProcedure).ConfigureAwait(false);
+                connection.Close();
+
+                return result.ToAsyncEnumerable();
+            }
+        }
+
+        public async Task<T> GetByIdAsync<T>(int id)
+        {
+            using (IDbConnection connection = Connection)
+            {
+                connection.Open();
+                var result = await connection.QueryFirstOrDefaultAsync<T>("spJobCRUD", new { Id = id > 0 ? id : 0, StatementType = "Select" }, commandType: CommandType.StoredProcedure).ConfigureAwait(false);
+                connection.Close();
+
+                return result;
+            }
+        }
+
+        public async Task<int> GetAllCountBy(object parameters)
+        {
+            var param = new DynamicParameters();
+            var model = new { StatementType = "GetAllCountBy", Id = 0 };
+            param.AddDynamicParams(model);
+            param.AddDynamicParams(parameters);
+
+            using (IDbConnection connection = Connection)
+            {
+                connection.Open();
+                var result = await connection.ExecuteScalarAsync<int>("spJobCRUD", param, commandType: CommandType.StoredProcedure).ConfigureAwait(false);
+                connection.Close();
+
+                return result;
+            }
+        }
+
+        public async Task<bool> AddRatingToJobs(object parameters)
+        {
+            var param = new DynamicParameters();
+            var model = new { StatementType = "AddRatingToJobs" };
+            param.AddDynamicParams(model);
+            param.AddDynamicParams(parameters);
+
+            using (IDbConnection connection = Connection)
+            {
+                connection.Open();
+                var result = await connection.ExecuteAsync("spJobCRUD", param, commandType: CommandType.StoredProcedure).ConfigureAwait(false);
+                connection.Close();
+
+                return result > 0;
+            }
+        }
+public enum JobCrudActionEnum : int
     {
         None = 0,
-        
+
         [Display(Name = "Create")]
         Create = 1,
+
         [Display(Name = "Update")]
         Update = 2,
+
         [Display(Name = "Delete")]
         Delete = 3,
-        [Display(Name = "GetAll")]
-        GetAll = 4
+
+        [Display(Name = "UpdatePromotion")]
+        UpdatePromotion = 4,
+
+        [Display(Name = "RefreshDate")]
+        RefreshDate = 5,
+
+        [Display(Name = "UpdateUser")]
+        UpdateUser = 6
+    }
+
+    public enum JobGetActionEnum : int
+    {
+        None = 0,
+
+        [Display(Name = "GetAllFiltering")]
+        GetAllFiltering = 1,
+
+        [Display(Name = "GetAllBy")]
+        GetAllBy = 2,
+
+        [Display(Name = "GetAllForDashboard")]
+        GetAllForDashboard = 3
     }
