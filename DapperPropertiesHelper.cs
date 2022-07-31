7896c207f3e1a806b9536a5a586a82c85f242fea
WingmanColt <supp.alliedsource@gmail.com>
2022-07-31 10:46:57 +0300

    Update DapperPropertiesHelper.cs

--- DapperPropertiesHelper.cs
+++ DapperPropertiesHelper.cs
@@ -4,12 +4,7 @@
 
 public static class DapperPropertiesHelper
     {
-        public static object GetPropValue(object src, string propName)
-        {
-            return src.GetType().GetProperty(propName).GetValue(src, null);
-        }
-
-        public static DynamicParameters AutoParameterFind(object Model)
+       public static DynamicParameters AutoParameterFind(object Model, string? SkipByAttributeName)
         {
             var parameters = new DynamicParameters();
             var prop = Model?.GetType().GetProperties();
@@ -22,7 +17,10 @@ public static class DapperPropertiesHelper
                 propValue = GetPropValue(Model, prop[i].Name);
                 if (propValue is int ? (Int32.TryParse(propValue.ToString(), out number) && number > 0) : propValue is not null)
                 {
-                    parameters.Add(prop[i].Name, propValue/*, GetPropertyType(prop[i].PropertyType.Name)*/);
+                    if(SkipByAttributeName is not null && !HasAttribute(prop[i], SkipByAttributeName))
+                    {
+                        parameters.Add(prop[i].Name, propValue/*, GetPropertyType(prop[i].PropertyType.Name)*/);
+                    }
                 }
             }
 
@@ -41,5 +39,15 @@ public static class DapperPropertiesHelper
                 default: return DbType.Object;
             }
 
+        }
+        public static object GetPropValue(object src, string propName)
+        {
+            return src.GetType().GetProperty(propName).GetValue(src, null);
+        }
+
+        public static bool HasAttribute(this PropertyInfo target, string atrName)
+        {
+            var attribs = target.GetCustomAttributesData().Any(x => x.AttributeType.Name == atrName + "Attribute");
+            return attribs;
         }
     }
